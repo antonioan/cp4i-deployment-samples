bb2e400ba4207f9a9064ceaa43f93aee24ca2ca0
Antonio Abu Nassar <Antonio.Abu.Nassar@ibm.com>
2023-10-25 15:35:17 +0300

    Update build-and-deploy script

--- TestgenBookshopAPI/service/build-and-deploy.sh
+++ TestgenBookshopAPI/service/build-and-deploy.sh
@@ -2,16 +2,27 @@
 
 runsudo=0
 tool=docker
+tag=latest
 
-delete=1
+terminate=0
 unnetwork=0
 
 network=0
-build=1
-deploy=1
+build=0
+deploy=0
 
-postdelete=0
-postunnetwork=0
+function do_build() {
+    name="$1"
+    src_dir="$2"
+    $tool build -t "$name:$tag" --build-arg SRC_DIR="$src_dir" .
+}
+
+function do_run() {
+    name="$1"
+    label="$2"
+    port="$3"
+    $tool run -dit --rm --net bookshop-network -p $port:5000 --name "$label" "$name:$tag"
+}
 
 if ! command -v "$tool" &> /dev/null; then
     echo "$tool could not be found"
@@ -19,7 +30,7 @@ if ! command -v "$tool" &> /dev/null; then
 fi
 
 if ! [[ "$PWD" == */TestgenBookshopAPI/service ]]; then
-    echo "you must run this command from TestgenBookshopAPI/service"
+    echo "you must run this command from inside TestgenBookshopAPI/service"
     exit 2
 fi
 
@@ -28,12 +39,8 @@ if [ "$runsudo" -ne 0 ]; then
     tool="sudo $tool"
 fi
 
-if [ "$delete" -ne 0 ]; then
-    $tool kill \
-        books-service \
-        customer-order-service \
-        bookshop-services \
-        gateway-service
+if [ "$terminate" -ne 0 ]; then
+    $tool kill books-service customer-order-service bookshop-services gateway-service
 fi
 
 if [ "$unnetwork" -ne 0 ]; then
@@ -41,67 +48,23 @@ if [ "$unnetwork" -ne 0 ]; then
 fi
 
 if [ "$network" -ne 0 ]; then
-    $tool network create --attachable bookshop-network
+    if [ "$tool" -eq "docker" ]; then
+        $tool network create --attachable bookshop-network
+    else
+        $tool network create bookshop-network
+    fi
 fi
 
 if [ "$build" -ne 0 ]; then
-    $tool build \
-        -t bookshop-books-service:latest \
-        --build-arg SRC_DIR=books-microservice \
-        .
-
-    $tool build \
-        -t bookshop-customer-service:latest \
-        --build-arg SRC_DIR=customer-microservice \
-        .
-
-    $tool build \
-        -t bookshop-services:latest \
-        --build-arg SRC_DIR=services \
-        .
-
-    $tool build \
-        -t bookshop-gateway-service:latest \
-        --build-arg SRC_DIR=gateway-service \
-        .
+    do_build "bookshop-books-service" "books-microservice"
+    do_build "bookshop-customer-service" "customer-microservice"
+    do_build "bookshop-services" "services"
+    do_build "bookshop-gateway-service" "gateway-service"
 fi
 
 if [ "$deploy" -ne 0 ]; then
-    $tool run -dit --rm \
-        --net  bookshop-network \
-        -p     6001:5000 \
-        --name books-service \
-        --env  ALL_LANGUAGES=en \
-        bookshop-books-service:latest
-
-    $tool run -dit --rm \
-        --net  bookshop-network \
-        -p     6002:5000 \
-        --name customer-order-service \
-        bookshop-customer-service:latest
-
-    $tool run -dit --rm \
-        --net  bookshop-network \
-        -p     6003:5000 \
-        --name bookshop-services \
-        bookshop-services:latest
-
-    $tool run -dit --rm \
-        --net  bookshop-network \
-        -p     6000:5000 \
-        --name gateway-service \
-        bookshop-gateway-service:latest
-
-fi
-
-if [ "$postdelete" -ne 0 ]; then
-    $tool kill \
-        books-service \
-        customer-order-service \
-        bookshop-services \
-        gateway-service
-fi
-
-if [ "$postunnetwork" -ne 0 ]; then
-    $tool network rm bookshop-network
+    do_run "bookshop-books-service" "books-service" 5001
+    do_run "bookshop-customer-service" "customer-order-service" 5002
+    do_run "bookshop-services" "bookshop-services" 5003
+    do_run "bookshop-gateway-service" "gateway-service" 5000
 fi
